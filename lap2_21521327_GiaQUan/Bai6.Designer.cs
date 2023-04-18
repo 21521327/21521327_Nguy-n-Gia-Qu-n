512f040421be7d2fe802520665f78a6771f47eb9
21521327 <21521327@gm.uit.edu.vn>
2023-04-18 20:58:48 +0700

    lap2

---
+++ lap2_21521327_GiaQUan/Bai6.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace lap2_21521327_GiaQUan
+{
+    partial class Bai6
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tv_file = new System.Windows.Forms.TreeView();
+            this.gb_text_pnj = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // tv_file
+            // 
+            this.tv_file.Location = new System.Drawing.Point(30, 78);
+            this.tv_file.Name = "tv_file";
+            this.tv_file.Size = new System.Drawing.Size(284, 345);
+            this.tv_file.TabIndex = 1;
+            this.tv_file.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.tv_file_AfterSelect);
+            // 
+            // gb_text_pnj
+            // 
+            this.gb_text_pnj.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.gb_text_pnj.Location = new System.Drawing.Point(344, 58);
+            this.gb_text_pnj.Name = "gb_text_pnj";
+            this.gb_text_pnj.Size = new System.Drawing.Size(435, 365);
+            this.gb_text_pnj.TabIndex = 3;
+            this.gb_text_pnj.TabStop = false;
+            this.gb_text_pnj.Text = "Nội dung";
+            // 
+            // Bai6
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gb_text_pnj);
+            this.Controls.Add(this.tv_file);
+            this.Name = "Bai6";
+            this.Text = "Bai6";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView tv_file;
+        private System.Windows.Forms.GroupBox gb_text_pnj;
+    }
+}
---
+++ lap2_21521327_GiaQUan/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lap2_21521327_GiaQUan
+{
+    public partial class bt_b6 : Form
+    {
+        public bt_b6()
+        {
+            InitializeComponent();
+        }
+
+        private void bt_b1_Click(object sender, EventArgs e)
+        {
+            bai1 bai1 = new bai1();
+            bai1.Show();
+        }
+
+        private void bt_bai2_Click(object sender, EventArgs e)
+        {
+            bai2 bai2 = new bai2();
+            bai2.Show();    
+        }
+
+        private void bt_bai3_Click(object sender, EventArgs e)
+        {
+            bai3 bai3 = new bai3();
+            bai3.Show();
+        }
+
+        private void bt_bai6_Click(object sender, EventArgs e)
+        {
+            bai5 bai5   = new bai5();
+            bai5.Show();
+        }
+
+        private void bt_bai4_Click(object sender, EventArgs e)
+        {
+            bai4 bai4  = new bai4();
+            bai4.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bai6 bai6 = new Bai6();
+            bai6.Show();
+        }
+    }
+}
